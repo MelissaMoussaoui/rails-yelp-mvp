37c2c059f0d2b7760a8e18e037d56947525d2a7b
Melissa Moussaoui <moussaoui.melissa@gmail.com>
2022-11-16 14:49:51 +0100

    marche pas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+# Review.destroy_all
+
+puts "Creating restaurants..."
+mcdo = {name: "mcdo", address: "16 rue Paradis", phone_number: "0606060606", category: "french"}
+bk = {name: "burger king", address: "16 rue Picasso", phone_number: "0707070707", category: "belgian"}
+kfc = {name: "kfc", address: "16 rue Picolo", phone_number: "0605040302", category: "italian"}
+sushi = {name: "sushi shop", address: "16 rue Poiscaille", phone_number: "0607080909", category: "japanese"}
+nui = {name: "nui", address: "16 rue Pimento", phone_number: "0604030806", category: "chinese"}
+
+restaurant_seed = [mcdo, bk, kfc, sushi, nui]
+
+# puts "Generating reviews..."
+# toto = {rating: 5, content: "sooooo good"}
+# tata = {rating: 4, content: "very nice!"}
+# tutu = {rating: 3, content: "nice but not woah"}
+# titi = {rating: 0, content: "horrible!"}
+# momo = {rating: 2, content: "not good at all but nice view"}
+# mama = {rating: 1, content: "one star for the place"}
+# mimi = {rating: 4, content: "excellent!"}
+# mymy = {rating: 5, content: "very excellent!"}
+
+#reviews_seed = [toto, tata, tutu, titi, momo, mama, mimi, mymy]
+
+restaurant_seed.each do |attributes|
+    restaurant = Restaurant.create!(attributes)
+    puts "Created #{restaurant.name}"
+end
+puts "finished!"
